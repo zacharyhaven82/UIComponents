880df759c23f047d75a7309d8d3af53da80355c0
Zachary Haven <zacharyhaven@me.com>
2025-07-15 11:58:14 -0500

    Make init public

--- Sources/UIComponents/ButtonDesignToken.swift
+++ Sources/UIComponents/ButtonDesignToken.swift
@@ -13,6 +13,12 @@ public struct ButtonDesignToken: Theming.ButtonThemeable, Sendable {
     public var foregroundColor: Color
     public var cornerRadius: CGFloat
     
+    public init(backgroundColor: Color, foregroundColor: Color, cornerRadius: CGFloat) {
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.cornerRadius = cornerRadius
+    }
+    
     static let defaultButtong = ButtonDesignToken(backgroundColor: Color(Theming.StyleDictionaryEnum.buttonPrimaryBackground),
                                                   foregroundColor: Color(Theming.StyleDictionaryEnum.buttonPrimaryText),
                                                   cornerRadius: CGFloat(Theming.StyleDictionaryEnum.buttonBorderRadius))
